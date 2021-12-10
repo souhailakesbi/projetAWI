8df416c732f3812d68d6403838ea3c148bddd83e
souhailakesbi <74243546+souhailakesbi@users.noreply.github.com>
2021-12-10 18:10:29 +0100

    Second Commit !

--- RecetteApp/src/app/app.module.ts
+++ RecetteApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecetteListComponent } from './components/HomePage/recette-list/recette-list.component';
+import { CreerFicheComponent } from './components/HomePage/creer-fiche/creer-fiche.component';
+import { CreerEtapeComponent } from './components/HomePage/creer-etape/creer-etape.component';
+import { MenuComponent } from './components/HomePage/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecetteListComponent,
+    CreerFicheComponent,
+    CreerEtapeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
