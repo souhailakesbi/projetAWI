e89eb7ba3a160e7b83ee05f4b6b0b25fd128a11a
souhailakesbi <74243546+souhailakesbi@users.noreply.github.com>
2022-01-01 12:03:46 +0100

    Ajout du module, component et service opour la gestion des stocks

---
+++ RecetteApp/src/app/components/HomePage/modification-stock/modification-stock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modification-stock',
+  templateUrl: './modification-stock.component.html',
+  styleUrls: ['./modification-stock.component.css']
+})
+export class ModificationStockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
