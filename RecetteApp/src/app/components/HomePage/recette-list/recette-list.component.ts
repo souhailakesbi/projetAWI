6b337c6131d9877ee720d3b5831eaa14b21c667e
souhailakesbi <74243546+souhailakesbi@users.noreply.github.com>
2022-01-09 22:14:51 +0100

    Merge + diff categorie + etapes reglages

--- RecetteApp/src/app/components/HomePage/recette-list/recette-list.component.ts
+++ RecetteApp/src/app/components/HomePage/recette-list/recette-list.component.ts
@@ -11,7 +11,7 @@ import {Router} from "@angular/router";
 })
 export class RecetteListComponent implements OnInit {
   fiches!: Fiche[];
-
+  searchText: any;
   constructor(public ficheService : AjoutFicheService,
   private router: Router) {}
 
